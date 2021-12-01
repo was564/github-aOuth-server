6967e6c253002113aa29759869504cc501f8de12
feroid765 <feroid765@gmail.com>
2021-12-02 02:08:14 +0900

    Init & GET /repos 스켈레톤
    레포의 목록을 불러오는 API의 스켈레톤.

---
+++ Classes/AuthUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Was564.MarkdownEditor.Function
+{
+    public static class AuthUtils
+    {
+        public static string GetTokenFromRequest(HttpRequest req)
+        {
+            try
+            {
+                // req.Headers["Authorization"] => Bearer someSecretAccessToken
+                var pureInput = req.Headers["Authorization"].ToString();
+                return pureInput.Split(" ")[1];
+            }
+            catch
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
+
---
+++ GetRepos.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Octokit;
+
+namespace Was564.MarkdownEditor.Function
+{
+    public class GetRepos
+    {
+        private readonly ILogger<GetRepos> _logger;
+
+        public GetRepos(ILogger<GetRepos> log)
+        {
+            _logger = log;
+        }
+
+        [FunctionName("GetRepos")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.Http, Name = "access_token", In = OpenApiSecurityLocationType.Header, Scheme = OpenApiSecuritySchemeType.Bearer)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(Dictionary<string, string>), Description = "사용자가 엑세스할 수 있는 모든 Repository를 표현하는 Dicitonary 입니다. Key는 레포의 ID를, Value는 레포의 이름을 나타냅니다.")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "text/plain", bodyType: typeof(string), Description = "헤더에 제공된 토큰이 잘못된 방식으로 되어 있습니다.")]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.Unauthorized, Description = "인증에 실패하였습니다.")]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.InternalServerError, Description = "이 서버가 맛이 갔거나 Github 서버가 맛이 갔습니다.")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "repo")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var token = string.Empty;
+            try
+            {
+                token = AuthUtils.GetTokenFromRequest(req);
+            }
+            catch
+            {
+                return new BadRequestObjectResult("Invalid Token");
+            }
+
+            var client = new GitHubClient(new ProductHeaderValue("was564"))
+            {
+                Credentials = new Credentials(token)
+            };
+
+            try
+            {
+                var repos = await client.Repository.GetAllForCurrent();
+                var result = repos.ToDictionary(repo => repo.Id, repo => repo.FullName);
+
+                return new OkObjectResult(result);
+            }
+            catch (AuthorizationException e)
+            {
+                return new UnauthorizedResult();
+            }
+            catch (ApiException e)
+            {
+                return new ContentResult() { StatusCode = 500 };
+            }
+        }
+    }
+}
+
